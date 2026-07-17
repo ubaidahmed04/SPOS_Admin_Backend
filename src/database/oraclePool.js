399171999573a59d807aca1b599d0f91c904e0e3
ubaidahmed04 <obaidkhanzada2004@gmail.com>
2026-07-17 10:42:15 +0500

    update Oracle client library path for production environment

--- src/database/oraclePool.js
+++ src/database/oraclePool.js
@@ -11,7 +11,7 @@ oracledb.fetchAsString = [oracledb.CLOB];
 try {
   oracledb.initOracleClient({
     libDir: process.env.NODE_ENV === 'production' 
-      ? "/cloudclusters/instantclient_23_5" 
+      ? "/opt/oracle/instantclient_23_7" 
       : "../Oracle/instantclient_21_3"
   });
 } catch (err) {
